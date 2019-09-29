b1b53ee52ab8abe3e04f90d8dbf7c8336f634730
atokb <ato15001@byui.edu>
2019-09-28 18:06:11 -0600

    nav2

--- web/header.php
+++ web/header.php
@@ -1,3 +1,3 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="home.php">King FiiIV</a>
+  <a class="navbar-brand mx-auto" href="home.php">King FiiIV</a>
 </nav>
