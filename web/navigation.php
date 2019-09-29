79ff7eee2e4f35d538af59e6272dc016fec412ea
atokb <ato15001@byui.edu>
2019-09-28 18:39:13 -0600

    fix

--- web/navigation.php
+++ web/navigation.php
@@ -1,5 +1,5 @@
 <div class="text-center mt-4 mb-4">
-    <a href="index.php" <?php if($currentPage == 'home') echo 'bg-primary'; ?>" class="btn btn-light btn-lg btn-space active" role="button" aria-pressed="true">Efie(Home) <span class="sr-only">(current)</span></a>
+    <a href="index.php" <?php if($currentPage == 'home') echo 'btn-primary'; ?>" class="btn btn-light btn-lg btn-space active" role="button" aria-pressed="true">Efie(Home) <span class="sr-only">(current)</span></a>
     <a href="assignments.php" <?php if($currentPage == 'assignments') echo 'btn-primary'; ?>" class="btn btn-light btn-lg btn-space active" role="button" aria-pressed="true">Adwuma(Assignments)</a>
 </div>
       
