a5f814ca2ba027e39204f1b2710741cdad50e7f3
atokb <ato15001@byui.edu>
2019-09-28 18:02:38 -0600

    add

--- web/navigation.php
+++ web/navigation.php
@@ -1,3 +1,5 @@
-
-      <a href="home.php" <?php if($currentPage == 'home') echo 'bg-info'; ?>" class="btn btn-primary btn-lg btn-space active" role="button" aria-pressed="true">Efie(Home) <span class="sr-only">(current)</span></a>
-      <a href="assignments.php" <?php if($currentPage == 'assignments') echo 'bg-info'; ?>" class="btn btn-primary btn-lg btn-space active" role="button" aria-pressed="true">Adwuma(Assignments)</a>
+<div class="d-flex justify-content-between">
+    <a href="home.php" <?php if($currentPage == 'home') echo 'bg-info'; ?>" class="btn btn-primary btn-lg btn-space active" role="button" aria-pressed="true">Efie(Home) <span class="sr-only">(current)</span></a>
+    <a href="assignments.php" <?php if($currentPage == 'assignments') echo 'bg-info'; ?>" class="btn btn-primary btn-lg btn-space active" role="button" aria-pressed="true">Adwuma(Assignments)</a>
+</div>
+      
