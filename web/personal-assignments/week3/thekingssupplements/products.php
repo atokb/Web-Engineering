f95167b1a4189e6d00c229997b1eeb16a322cd0b
atokb <ato15001@byui.edu>
2019-10-01 15:21:27 -0600

    week 3 on

---
+++ web/personal-assignments/week3/thekingssupplements/products.php
@@ -0,0 +1,9 @@
+<?php 
+    $products = array("LEAN N' SASSY", "SCORCHED", "IGNITOR", "HEX 6000 CREATINE", "FEMME FIT", "GROW MASS GAINER");
+    // $images = array()
+
+    for($x = 0, $x < $products.length, $++) {
+        echo $products[$x];
+        echo "<br>"
+    }
+?>
