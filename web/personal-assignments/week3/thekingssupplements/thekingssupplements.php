b188774f73dca1be4d4b7af2a4fe5ecc7fa21aa9
atokb <ato15001@byui.edu>
2019-10-01 15:48:44 -0600

    on

--- web/personal-assignments/week3/thekingssupplements/thekingssupplements.php
+++ web/personal-assignments/week3/thekingssupplements/thekingssupplements.php
@@ -9,9 +9,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>The King's Supplements</title>
 </head>
-<body>
+<body class="jumbotron">
     <header>
         <?php include('header.php');
         ?>
@@ -20,7 +21,7 @@
     <?php include('products.php');
     ?>
 
-    
+
    
 </body>
 </html>
