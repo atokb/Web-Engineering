eef1759a8269af7984a65fd49faa9909d40a481c
atokb <ato15001@byui.edu>
2019-10-01 21:43:47 -0600

    okay

--- web/personal-assignments/week3/thekingssupplements/products.php
+++ web/personal-assignments/week3/thekingssupplements/products.php
@@ -1,13 +1,58 @@
 <?php 
-    $products = array("LEAN N' SASSY", "SCORCHED", "IGNITOR", "HEX 6000 CREATINE", "FEMME FIT", "GROW MASS GAINER");
+    $items = array("LEAN N' SASSY", "SCORCHED", "IGNITOR", "HEX 6000 CREATINE", "FEMME FIT", "GROW MASS GAINER");
     $images = array("lean-sassy", "Scorched", "Ignitor", "Hex-", "Femme-Fit", "Grow");
     foreach(images as image) {
         echo "<img src='/img/$image.jpg'> <br>";
     }
-    $prices = array("$59.95", "$54.95", "$60", "$34.95", "$69.95", "$79.95");
+    $price = array("$59.95", "$54.95", "$60", "$34.95", "$69.95", "$79.95");
 
-    for($x = 0, $x < $products.length, $++) {
-        echo $products[$x];
-        echo "<br>"
+    //Load Session
+if (!isset($_SESSION["total"]) ) {
+    $_SESSION["total"] = 0;
+    for ($i=0; $i< count($items); $i++) {
+      $_SESSION["images"][$i] = 0;
+      $_SESSION["qty"][$i] = 0;    
+      $_SESSION["price"][$i] = 0;
     }
+  }
+  
+  //Reset
+  if ( isset($_GET['reset']) )
+  {
+  if ($_GET["reset"] == 'true')
+    {
+    unset($_SESSION["qty"]);
+    unset($_SESSION["images"]);
+    unset($_SESSION["price"]);
+    unset($_SESSION["total"]);
+    unset($_SESSION["cart"]);
+    }
+  }
+  
+  //Add
+  if ( isset($_GET["add"]) )
+  {
+  $i = $_GET["add"];
+  $qty = $_SESSION["qty"][$i] + 1;
+  $_SESSION["price"][$i] = $price[$i] * $qty;
+  $_SESSION["cart"][$i] = $i;
+  $_SESSION["qty"][$i] = $qty;
+  }
+  
+  //Delete
+  if ( isset($_GET["delete"]) )
+  {
+  $i = $_GET["delete"];
+  $qty = $_SESSION["qty"][$i];
+  $qty--;
+  $_SESSION["qty"][$i] = $qty;
+  if ($qty == 0) {
+   $_SESSION["price"][$i] = 0;
+   unset($_SESSION["cart"][$i]);
+  }
+  else
+  {
+  $_SESSION["price"][$i] = $price[$i] * $qty;
+  }
+  }
 ?>
--- web/personal-assignments/week3/thekingssupplements/thekingssupplements.php
+++ web/personal-assignments/week3/thekingssupplements/thekingssupplements.php
@@ -1,6 +1,8 @@
 <?php 
-    SESSION_START();
+    session_start ();
     $currentPage = 'supplementHome';
+
+    include('products.php');
 ?>
 
 <!DOCTYPE html>
@@ -10,26 +12,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css">
     <title>The King's Supplements</title>
 </head>
 <body class="jumbotron">
-    <header>
         <?php include('header.php');
         ?>
-    </header>
-
-    <?php 
-    
-    include("products.php");
-    echo $products;
-
-    $images = array("lean-sassy", "Scorched", "Ignitor", "Hex-", "Femme-Fit", "Grow");
-    foreach(images as image) {
-        echo "<img src='/img/$image.jpg'> <br>";
-    }
-    ?>
-
-
-   
+   <div>
+   <h2>Products</h2>
+   <?php 
+        for ($i=0; $i < count($items); $i++) {
+   ?>
+    <ul>
+        <?php echo "<li>$items[$i]</li><br>"; ?>
+    </ul>
+   </div>
 </body>
 </html>
