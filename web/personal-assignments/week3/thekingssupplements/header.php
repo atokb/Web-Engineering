bca199c5858f2044682dd35e184b111a5da73041
atokb <ato15001@byui.edu>
2019-10-02 20:16:13 -0600

    new

--- web/personal-assignments/week3/thekingssupplements/header.php
+++ web/personal-assignments/week3/thekingssupplements/header.php
@@ -1,3 +1,3 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand mx-auto" href="thekingssupplements.php">The King's Supplements</a>
+  <a class="btn btn-primary navbar-brand mx-auto" href="thekingssupplements.php">The King's Supplements</a>
 </nav>
