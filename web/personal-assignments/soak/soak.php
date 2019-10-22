5a8f4de43a1bd2dbbf84d4ad29e15009d8bc13eb
atokb <ato15001@byui.edu>
2019-10-22 17:17:52 -0600

    mess

--- web/personal-assignments/soak/soak.php
+++ web/personal-assignments/soak/soak.php
@@ -51,7 +51,7 @@ if(count($_POST)>0) {
 					<span class="login100-form-title p-b-48">
 						<i class="zmdi zmdi-font"></i>
 					</span>
-					<div><?php $message ?></div>
+					<div class="message"><?php if($message!="") { echo $message; } ?></div>
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is: a@b.c">
 						<input class="input100" type="text" name="email" required>
 						<span class="focus-input100" data-placeholder="Email"></span>
