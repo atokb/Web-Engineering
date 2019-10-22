380b765da6bb9b1fe15c069f29adaa21e8dbe8ab
atokb <ato15001@byui.edu>
2019-10-22 17:38:18 -0600

    autentication check

--- web/personal-assignments/soak/soak.php
+++ web/personal-assignments/soak/soak.php
@@ -52,12 +52,14 @@ if(count($_POST)>0) {
 						<i class="zmdi zmdi-font"></i>
 					</span>
 					<div class="message"><?php if($message!="") { echo $message; } ?></div>
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is: a@b.c">
+					<div class="wrap-input100 validate-input">
+					<!-- <div class="wrap-input100 validate-input" data-validate = "Valid email is: a@b.c"> -->
 						<input class="input100" type="text" name="email" required>
 						<span class="focus-input100" data-placeholder="Email"></span>
 					</div>
 
-					<div class="wrap-input100 validate-input" data-validate="Enter password">
+					<div class="wrap-input100 validate-input">
+					<!-- <div class="wrap-input100 validate-input" data-validate="Enter password"> -->
 						<span class="btn-show-pass">
 							<i class="zmdi zmdi-eye"></i>
 						</span>
