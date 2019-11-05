10653fbb9668a2e312d510d7d1a98f30c49eddd8
atokb <ato15001@byui.edu>
2019-11-05 15:09:12 -0700

    work

--- web/personal-assignments/soak/soak_routine.php
+++ web/personal-assignments/soak/soak_routine.php
@@ -40,7 +40,7 @@ include 'db.php';
               <?php $statement = $db->query('SELECT routine_name, routine_instructions FROM routines');
                 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
                 {
-                echo '<h2> Routine<br> ' . $row["routine_name"] .'</h2>' .  '<h4>Instructions</h4><p>' . $row["routine_instructions"] . '</p>';
+                echo '<h2> Routine<br> ' . $row["routine_name"] .'</h2>' .  '<h4>Instructions</h4><p>' . $row["routine_instructions"] . '</p><br><br>';
                 }
               ?></i>
           </span>
