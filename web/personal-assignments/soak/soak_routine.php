c4bfc13538f5a362ffdeb5a3c45d31d74864b1ef
atokb <ato15001@byui.edu>
2019-11-07 21:37:06 -0700

    okay

--- web/personal-assignments/soak/soak_routine.php
+++ web/personal-assignments/soak/soak_routine.php
@@ -31,7 +31,7 @@ include 'db.php';
     <div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
-				<form class="login100-form validate-form" method="post" action="soak_start.php">
+				<form class="login100-form validate-form" method="post" action="">
 					<span class="login100-form-title p-b-26">
 						Here is Your Current Royal Routine
 					</span>
@@ -59,7 +59,7 @@ include 'db.php';
           <div class="container-login100-form-btn">
 						<div class="wrap-login100-form-btn">
               <div class="login100-form-bgbtn"></div>
-							<button class="login100-form-btn" type="submit" name="change">Change Goal</button>
+							<button class="login100-form-btn" type="submit" name="change"><a href="soak_start.php">Change Goal</a></button>
 						</div>
 					</div>
 
