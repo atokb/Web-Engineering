47208211d6cc5db8ee5155d6ee761b184faee391
atokb <ato15001@byui.edu>
2019-11-08 17:52:03 -0700

    seeing changes

--- web/personal-assignments/soak/soak_edit.php
+++ web/personal-assignments/soak/soak_edit.php
@@ -3,6 +3,29 @@ $currentPage = 'soak-edit';
 
 include 'db.php';
 
+if (isset($_POST['routineName']))
+{
+	$routinename = $_POST['routineName'];
+	$instructions = $_POST['instructions'];
+
+try{
+$stmt = $db->prepare('UPDATE routines SET routine_name = :routine_name, routine_instructions = :routine_instructions');
+$stmt->execute(array(':routine_name' => $routinename, ':routine_instructions' => $instructions));
+
+
+if($stmt->rowCount() == 1){
+	$message = "Routine Successfully Updated<br><br>
+	Go to <a href='soak_routine.php'>Routines</a>
+    ";
+    header('location: soak-routine.php');
+}
+}
+catch(PDOException $ex){
+	$erMessage = "An error occured".$ex->getMessage();
+}
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -41,20 +64,27 @@ include 'db.php';
               <?php $statement = $db->query('SELECT routine_name, routine_instructions FROM routines');
                 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
                 {
-                echo '<h2>' . $row["routine_name"] .'</h2>' .  '<h4>Instructions</h4><p>' . $row["routine_instructions"] . '<div class="container-login100-form-btn">
-                <div class="wrap-login100-form-btn">
-                    <button class="btn btn-danger btn-sm" type="" name="delete">Delete</button>
+                echo '<div class="wrap-input100 validate-input">
+                <input class="input100" type="text" name="routineName">
+                <span class="focus-input100" data-placeholder="Routine Name">' . $row["routine_name"] . '</span>
+            </div>
+
+            <div class="form-group shadow-textarea">
+                <span>Instructions</span>
+                <textarea class="form-control z-depth-1" id="instructions" name="instructions" rows="8">' . $row["routine_instructions"] . '</textarea>
+            </div>
+                    <button class="btn btn-danger btn-sm" type="submit" name="update">Update</button>
                 </div>
             </div></p><br>';
                 }
               ?></i>
           </span>
-          <div class="container-login100-form-btn">
+          <!-- <div class="container-login100-form-btn">
                 <div class="wrap-login100-form-btn">
                     <div class="login100-form-bgbtn"></div>
                     <button class="login100-form-btn" type="submit" name="update">Save Edits</button>
                 </div>
-            </div>
+            </div> -->
 
 				</form>
 			</div>
